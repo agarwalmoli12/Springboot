8057af3f94c5f330674fc362bd7dde79cee5866f
Moli Agarwal <moliagarwal77@gmail.com>
2026-07-20 18:37:27 +0530

    Day3

---
+++ Springboot/myfirstproject/src/main/java/com/firstproject/myfirstproject/MyfirstprojectApplication.java
@@ -0,0 +1,13 @@
+package com.firstproject.myfirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //[@Configuration + @EnableAutoConfiguration + @ComponentScan]
+public class MyfirstprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstprojectApplication.class, args);
+	}
+
+}
---
+++ Springboot/myfirstproject/src/main/java/com/firstproject/myfirstproject/cat.java
@@ -0,0 +1,9 @@
+package com.firstproject.myfirstproject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+@Component
+public class cat {
+    @Autowired
+    Dog dog;
+}
